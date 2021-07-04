9e4deb3307b2d38768d018323c80b3ac557879bb
алёша <asprimechaev@icloud.com>
2021-07-04 17:13:24 +0300

    Initial Commit

---
+++ Shoot/ShootApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShootApp.swift
+//  Shoot
+//
+//  Created by Alexey Primechaev on 7/4/21.
+//
+
+import SwiftUI
+
+@main
+struct ShootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
