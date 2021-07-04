6d5f8b0cdf4a86e29635394b0bf7af23ded76a6d
алёша <asprimechaev@icloud.com>
2021-07-04 18:12:00 +0300

    такс

--- Shoot/CameraView.swift
+++ Shoot/CameraView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct CameraView: View {
     @StateObject var model = CameraViewModel()
     
-    @State var currentZoomFactor: CGFloat = 1.0
     
     var captureButton: some View {
         Button(action: {
@@ -18,112 +17,56 @@ struct CameraView: View {
         }, label: {
             Circle()
                 .foregroundColor(.white)
-                .frame(width: 80, height: 80, alignment: .center)
-                .overlay(
-                    Circle()
-                        .stroke(Color.black.opacity(0.8), lineWidth: 2)
-                        .frame(width: 65, height: 65, alignment: .center)
-                )
+                .frame(width: 73, height: 73, alignment: .center)
+
         })
     }
     
-    var capturedPhotoThumbnail: some View {
-        Text("foto")
-//        Group {
-//            if model.photo != nil {
-//                Image(uiImage: model.photo.image!)
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fill)
-//                    .frame(width: 60, height: 60)
-//                    .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-//                    .animation(.spring())
-//
-//            } else {
-//                RoundedRectangle(cornerRadius: 10)
-//                    .frame(width: 60, height: 60, alignment: .center)
-//                    .foregroundColor(.black)
-//            }
-//        }
-    }
+
     
-    var flipCameraButton: some View {
-        Button(action: {
-            model.flipCamera()
-        }, label: {
-            Circle()
-                .foregroundColor(Color.gray.opacity(0.2))
-                .frame(width: 45, height: 45, alignment: .center)
-                .overlay(
-                    Image(systemName: "camera.rotate.fill")
-                        .foregroundColor(.white))
-        })
-    }
+
     
     var body: some View {
-        GeometryReader { reader in
-            ZStack {
-                Color.black.edgesIgnoringSafeArea(.all)
+
                 
+        ZStack {
                 VStack {
-                    Button(action: {
-                        model.switchFlash()
-                    }, label: {
-                        Image(systemName: model.isFlashOn ? "bolt.fill" : "bolt.slash.fill")
-                            .font(.system(size: 20, weight: .medium, design: .default))
-                    })
-                    .accentColor(model.isFlashOn ? .yellow : .white)
-                    
+                   
+                    Spacer()
                     CameraPreview(session: model.session)
-                        .gesture(
-                            DragGesture().onChanged({ (val) in
-                                //  Only accept vertical drag
-                                if abs(val.translation.height) > abs(val.translation.width) {
-                                    //  Get the percentage of vertical screen space covered by drag
-                                    let percentage: CGFloat = -(val.translation.height / reader.size.height)
-                                    //  Calculate new zoom factor
-                                    let calc = currentZoomFactor + percentage
-                                    //  Limit zoom factor to a maximum of 5x and a minimum of 1x
-                                    let zoomFactor: CGFloat = min(max(calc, 1), 5)
-                                    //  Store the newly calculated zoom factor
-                                    currentZoomFactor = zoomFactor
-                                    //  Sets the zoom factor to the capture device session
-                                    model.zoom(with: zoomFactor)
-                                }
-                            })
-                        )
+                        
+                        .aspectRatio(3/4, contentMode: .fit)
+
                         .onAppear {
                             model.configure()
                         }
-//                        .alert(isPresented: $model.showAlertError, content: {
-//                            Alert(title: Text(model.alertError.title), message: Text(model.alertError.message), dismissButton: .default(Text(model.alertError.primaryButtonTitle), action: {
-//                                model.alertError.primaryAction?()
-//                            }))
-//                        })
-//                        .overlay(
-//                            Group {
-//                                if model.willCapturePhoto {
-//                                    Color.black
-//                                }
-//                            }
-//                        )
-                        .animation(.easeInOut)
+
                     
+                    Spacer()
+                    GeometryReader { geometry in
+                        
+                            captureButton.position(x: geometry.size.width/2)
+                            
+                            Menu {
+                                
+                            } label: {
+                                Image(systemName: "ellipsis.circle.fill").font(.title).padding(28).foregroundColor(.white)
+                            }.position(x: geometry.size.width - (geometry.size.width - (geometry.size.width + 73)/2)/2)
+                        
+                        .frame(height: 73)
+                        .onAppear {
+                            print(geometry.size.width)
+                            print(geometry.size.height)
+                        }
+                        
+                    }.frame(height: 73)
+
+                    Spacer()
                     
-                    HStack {
-                        capturedPhotoThumbnail
-                        
-                        Spacer()
-                        
-                        captureButton
-                        
-                        Spacer()
-                        
-                        flipCameraButton
-                        
-                    }
-                    .padding(.horizontal, 20)
                 }
-            }
-        }
+            
+            
+        }.background(Color.black.edgesIgnoringSafeArea(.all))
+      
     }
 }
