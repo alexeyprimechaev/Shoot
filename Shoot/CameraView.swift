a9c7e520c115a4469cdf2707cc8d735fc3b78e57
алёша <asprimechaev@icloud.com>
2021-07-07 23:19:38 +0300

    РЕлИЗ с ВОТ ЭТИМ

--- Shoot/CameraView.swift
+++ Shoot/CameraView.swift
@@ -122,9 +122,9 @@ func changeIcon() {
     if UIDevice.modelName == "iPhone X" || UIDevice.modelName == "iPhone XS" || UIDevice.modelName == "iPhone XS Max" {
         icon = "X Lens"
     } else if UIDevice.modelName == "iPhone 11" || UIDevice.modelName == "iPhone 12" || UIDevice.modelName == "iPhone 12 mini" {
-        icon = nil
-    } else if UIDevice.modelName == "iPhone 12 Pro" || UIDevice.modelName == "iPhone 12 Pro Max" || UIDevice.modelName == "iPhone 11 Pro Max" || UIDevice.modelName == "iPhone 11 Pro"{
         icon = "3 Lens"
+    } else if UIDevice.modelName == "iPhone 12 Pro" || UIDevice.modelName == "iPhone 12 Pro Max" || UIDevice.modelName == "iPhone 11 Pro Max" || UIDevice.modelName == "iPhone 11 Pro"{
+        icon = nil
     } else if UIDevice.modelName == "iPhone XR" {
         icon = "XR Lens"
     } else {
